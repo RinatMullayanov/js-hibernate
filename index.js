13c4577f5a484b21200ae32447c563c0af1bf293
Rinat <nutsgamedev@gmail.com>
2014-08-10 22:38:11 +0600

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 var jsORM = {};
+var stringFormat = require('string-formatter');
 
-jsORM.TableMap = function (tableName) {
-    this.tableName = tableName;
-    return this;
+jsORM.tableMap = function(tableName) {
+    var self = this instanceof jsORM.tableMap ? this : Object.create(jsORM.tableMap.prototype);
+
+    self.tableName = tableName;
+    return self;
 }
 
-jsORM.TableMap.prototype.ColumnMap = function(objProperty, tableProperty) {
-	this.columnsMap = this.columnsMap || [];
-	this.columnsMap[objProperty] = tableProperty;
+jsORM.tableMap.prototype.columnMap = function(objProperty, tableProperty) {
+    this.columnMaps = this.columnMaps || {};
+    this.columnMaps[objProperty] = tableProperty;
 
-	return this;
+    return this;
 };
+
+jsORM.query = function(tableMap) {
+    // check instance 
+    if (tableMap instanceof jsORM.tableMap) {
+        var self = this instanceof jsORM.query ? this : Object.create(jsORM.query.prototype);
+
+        self.map = tableMap;
+        return self;
+    }
+}
+
+jsORM.query.prototype.select = function() {
+    // generate sql
+    var sqlQuery = 'select ';
+    var self = this;
+    var map = self.map.columnMaps;
+
+    for (var prop in map) {
+        if (!map.hasOwnProperty(prop)) continue;
+        sqlQuery += '{' + prop + '}, ';
+    }
+    // remove last comma
+    var lastComma = sqlQuery.lastIndexOf(',');
+    sqlQuery = sqlQuery.substring(0, lastComma);
+
+    return sqlQuery;
+}
 
 module.exports = jsORM;
--- test/test.js
+++ test/test.js
@@ -1,2 +1,11 @@
-var orm = require('../index.js');
+var jsORM = require('../index.js');
+
+var userMap = jsORM.tableMap('User')
+			.columnMap('id', 'id')
+			.columnMap('name', 'shortName')
+			.columnMap('phone', 'tel');
+
+var sqlQuery = jsORM.query(userMap).select();
+console.log(sqlQuery);
+
 console.log("check");
