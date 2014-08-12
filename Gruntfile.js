2abc3c4ba1a6d66bac6d9ca4aec1fd89aa59ac2f
Rinat <nutsgamedev@gmail.com>
2014-08-13 01:04:45 +0600

    add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			all: ['./*.js', 'test/**/*.js'],
+			options: {
+				globals: {
+					_: false,
+					$: false,
+					jasmine: false,
+					describe: false,
+					it: false,
+					expect: false,
+					beforeEach: false
+				},
+				browser: true,
+				devel: true
+			}
+		},
+		testem: {
+			unit: {
+				options: {
+					framework: 'jasmine2',
+					launch_in_dev: ['PhantomJS'],
+					before_test: 'grunt jshint',
+					serve_files: [
+						'./*.js',
+						'test/**/*.js'
+					],
+					watch_files: [
+						'./*.js',
+						'test/**/*.js'
+					]
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-testem')
+
+	grunt.registerTask('default', ['testem:run:unit']);
+};
